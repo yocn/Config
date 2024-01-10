58d26a2e157ec9ea2910dd79a0bd4c57afa034c4
yocn <zhaoyingkun@163.com>
2024-01-10 17:34:25 +0800

    update

--- cmd.sh
+++ cmd.sh
@@ -2,4 +2,29 @@
 # touch $file
 # ls > "$file"
 # mv $file config/
-yt-dlp "https://www.youtube.com/@AamchiMumbai/videos"
+# yt-dlp "https://www.youtube.com/@AamchiMumbai/videos"
+basePath=`pwd`
+list=$basePath"/"list.txt
+date=`date +%F`
+echo -o "\"/home/yocn/youtube/cron/$date/%(title)s-%(id)s.%(ext)s\"" > yt-dlp.conf
+
+videos="https://www.youtube.com/@AamchiMumbai/videos"
+
+if [ $# -gt 0 ]; then
+    echo "参数个数为$#个"
+    if [ $1 -eq 1 ]; then
+        yt-dlp --get-id --flat-playlist $videos | awk '{print "https://www.youtube.com/watch?v=" $0}' > $list
+    fi
+fi
+
+for url in `cat $list`
+do
+    echo "正在下载: $url"
+    sleep 1
+    # yt-dlp --config-location $basePath"/"yt-dlp.conf $url
+    sleep 10
+    sed -i "1,1d" $list
+    git add $list
+    git commit -m "$date complete a download $url"
+    git pull
+done
