13648dd5808f9ae5f55b71c0dac3abc982e64f95
yocn <zhaoyingkun@163.com>
2024-01-15 16:33:03 +0800

    2024-01-15 complete a download https://www.youtube.com/watch?v=THxR30mwDhI

--- cmd.sh
+++ cmd.sh
@@ -1,15 +1,22 @@
 # yt-dlp "https://www.youtube.com/@AamchiMumbai/videos"
 # yt-dlp -f 'wv[height=1920][ext=webm]+ba[ext=m4a]' "https://www.youtube.com/watch?v=wKwl9DNHOqQ"
+# https://www.youtube.com/watch?v=3_UrWv3hWe0
 
 basePath=`pwd`
+echo "开始执行cmd.sh-$basePath"
 list=$basePath"/"list.txt
+folderName="@AamchiMumbai"
+targetPath="/home/yocn/youtube/cron"
 date=`date +%F`
-echo -o "\"/home/yocn/youtube/cron/$date/%(title)s-%(id)s.%(ext)s\"" > yt-dlp.conf
+echo -o "\"$targetPath/$folderName/%(title)s-%(id)s.%(ext)s\"" > yt-dlp.conf
 
+conpleteListFile=$targetPath"/list.txt"
+
+init=0
 videos="https://www.youtube.com/@AamchiMumbai/videos"
 
-if [ $# -gt 0 ]; then
-    echo "参数个数为$#个"
+if [ $init -gt 0 ]; then
+    echo "执行init，获取所有视频列表。"
     if [ $1 -eq 1 ]; then
         yt-dlp --get-id --flat-playlist $videos | awk '{print "https://www.youtube.com/watch?v=" $0}' > $list
     fi
@@ -22,8 +29,11 @@ do
     if [ -e "$mutexFile" ]; then
         echo "正在下载: $url"
         sleep 1
-        yt-dlp -f 'wv[height=1920][ext=webm]+ba[ext=m4a]' --config-location $basePath"/"yt-dlp.conf $url
+        yt-dlp -f 'wv[height=256][ext=mp4]+ba[ext=m4a]' --config-location $basePath"/"yt-dlp.conf $url
+        echo "下载完成: $url"
+        # 从list.txt中删除当前被下载的url
         sed -i "1,1d" $list
+        echo $url >> $conpleteListFile
         git pull
         git add .
         git commit -m "$date complete a download $url"
